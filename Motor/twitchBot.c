e660b7c561cf766d0226add5816557fa3a83cb51
jillytot <jill@aylogames.com>
2014-06-18 13:06:12 -0700

    Added Drive Speed / Turn speed changes

--- Motor/twitchBot.c
+++ Motor/twitchBot.c
@@ -10,7 +10,9 @@ fdserial *term; //enables full-duplex serilization of the terminal (In otherwise
 int ticks = 12; //each tick makes the wheel move by 3.25mm, 64 ticks is a full wheel rotation (or 208mm)
 int turnTick = 6; //Turning is at half the normal rate
 int maxSpeed = 128; //the maximum amount of ticks the robot can travel with the "drive_goto" function
-int minSpeed = 1; //the lowest amount of ticks the robot can travel with the "drive_goto" function
+int minSpeed = 2; //the lowest amount of ticks the robot can travel with the "drive_goto" function
+int maxTurnSpeed = 64;
+int minTurnSpeed = 2;
 
 int main()
 {
@@ -27,43 +29,79 @@ int main()
 
   char c;
 
-  while (1){
+  while (1)
+  {
     c = fdserial_rxChar(term); //Get the character entered from the terminal
     if (c != -1)
     {
       //Use the below to see if any key input is being read by the terminal
       // dprint(term, "You typed: %c\n", c);
-      //Link key presses to directional commands, and print the command to strings
-      if (c == 'f') { //press "f" fof forward
-        dprint(term, "Foward, \n",c);
-        drive_goto(ticks, ticks);
-      } else if (c == 'b') { //press "b" for backward
-        dprint(term, "Backward, \n", c);
-        drive_goto(-ticks, -ticks);
-      } else if (c == 'r') { //press "r" for right turn
-        dprint(term, "Right, \n", c);
-        drive_goto(turnTick, -turnTick);
-      } else if (c == 'l') { //press "l" for left turn
-        dprint(term, "Left, \n", c);
-        drive_goto(-turnTick, turnTick);
 
-        //speeding up and slowing down
-      } else if (c == 'u') {
+      //Link key presses to directional commands, and print the command to strings
+      if (c == 'f') //press "f" fof forward
+      {           
+        dprint(term, "Foward, \n");
+        drive_goto(ticks, ticks);
+      } 
+      else if (c == 'b') //press "b" for backward
+      { 
+        dprint(term, "Backward, \n");
+        drive_goto(-ticks, -ticks);
+      } 
+      else if (c == 'r') //press "r" for right turn
+      { 
+        dprint(term, "Right, \n");
+        drive_goto(turnTick, -turnTick);
+      } 
+      else if (c == 'l') //press "l" for left turn
+      { 
+        dprint(term, "Left, \n");
+        drive_goto(-turnTick, turnTick);
+      } 
+
+      //Increasing and Decreasing Drive Speed
+      else if (c == 'u') 
+      {
         ticks = ticks +2;
-        turnTick = turnTick +1;
-        if (ticks > maxSpeed) {//clamp speed so it can't go over the maximum speed
+        if (ticks > maxSpeed) //clamp speed so it can't go over the maximum speed
+        {
            ticks = maxSpeed;
         }
         dprint(term, "Robot will now move %d ticks per second. \n", ticks);
-      } else if (c == 'd') {
+      } 
+      else if (c == 'd') 
+      {
         ticks = ticks - 2;
-        turnTick = turnTick -1;
-        if (ticks < minSpeed) {//clamp speed so it can't go negative or 0
+        if (ticks < minSpeed) //clamp speed so it can't go negative or 0
+        {
           ticks = minSpeed;
         }
         dprint(term, "Robot will now move %d ticks per second. \n", ticks);
       }
-    }
-  }
-}
+
+      //Increase Turn Speed;
+      else if (c == 't') 
+      {
+        turnTick = turnTick +1;
+        if (turnTick > maxTurnSpeed)
+        {
+          turnTick = maxTurnSpeed;
+        }
+        dprint(term, "Robot will now turn at %d ticks per second. \n", turnTick);  
+      }
+      
+      //decrease turn speed
+      else if (c == 'n')
+      {
+        turnTick = turnTick -1;
+        if (turnTick < minTurnSpeed)
+          {
+          turnTick = minTurnSpeed;
+          }
+        dprint(term, "Robot will now turn at %d ticks per second. \n", turnTick);
+      }
+
+    } // End of Read Character Function
+  } // End of While Loop
+} //End of Main Loop
 
