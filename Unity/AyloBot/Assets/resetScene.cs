2da75b74d6dfaae5dc81aa030c7df1487d80ba89
jillytot <jill@aylogames.com>
2014-07-28 15:47:31 -0700

    added reset scene

---
+++ Unity/AyloBot/Assets/resetScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resetScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.R)) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
