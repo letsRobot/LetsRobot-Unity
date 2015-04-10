d57ba2bf6e2b28d8d50c28640cea6a8bff030e35
jillytot <jill@aylogames.com>
2015-04-10 13:01:53 -0700

    You can now restart tracks, loop tracks, and step back in the songs.

--- Unity/AyloBot/Assets/jukeBox.cs
+++ Unity/AyloBot/Assets/jukeBox.cs
@@ -4,21 +4,58 @@ using System.Collections;
 public class jukeBox : MonoBehaviour {
 
 	public bool enableMusic; //enables music if true
+	public bool loopTrack;//loops the current track
+
+	bool triggerNextTrack;//Triggers next track if song is looping
 	public AudioClip[] Songs; //All Songs
 	int tracks; //Index of tracks in the JukeBox
-	AudioSource thisJukeBox; //References the audio source
+	AudioSource thisJukeBox; //References the audio source of this game Unity Game Object
 
-	bool Mute; //pauses current music
+	bool Mute; //Mutes the current track
 	bool nextTrack; //true if next track
 	bool skipTrack; //True if skipping to next track
-	int playTrack = -1; //index of next track to play
+	bool backTrack; //True if skpping backrward
+	
+	bool restartTrack; //trickers restarting a track
 
+	int playTrack = -1; //index of next track to play
 	float songLength; //Length of the current song
+	
+	void Awake () {
+
+		//Get the Audio Source assigned to this game object
+		thisJukeBox = this.gameObject.GetComponent<AudioSource>();
+		Mute = false;
+		enableMusic = true; //This should be tied to a static global variable at some point.
+		tracks = Songs.Length;
+		Debug.Log("Number of Songs: " + tracks);
+		nextTrack = false;
+		skipTrack = false;
+		loopTrack = false;
+		triggerNextTrack = false;
+		restartTrack = false;
+		backTrack = false;
+	}
+	
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//If music is enabled, play music, otherwise DONT!
+		if (enableMusic == true) {
+			playMusic();
+		} else {
+			thisJukeBox.Stop();
+		}
+	}
 
 	//Play Music
 	void playMusic() {
-
-		if (enableMusic == true && !thisJukeBox.isPlaying) {
+		
+		if (!thisJukeBox.isPlaying) {
 			//pick a song and play it!
 			AudioClip currentSong = Songs[newTrack()];
 			thisJukeBox.clip = currentSong;
@@ -26,37 +63,52 @@ public class jukeBox : MonoBehaviour {
 			Debug.Log("Song is Playing");
 			songLength = currentSong.length;
 			Debug.Log ("Track Length: " + songLength);
-
+			
 			//skip to the next song if "skipped"
 		} else if (skipTrack == true && thisJukeBox.isPlaying) {
 			thisJukeBox.Stop();
 			Debug.Log("Skipping to next track");
 			skipTrack = false;
 		}
-
+		
 		UMusicInput(); //Unity shortcuts for Music
 	}
-
+	
+	//In charge of track switching!
 	int newTrack() {
-
+		
 		int trackIndex = tracks - 1;
-		if (!thisJukeBox.isPlaying) {
+		if (!thisJukeBox.isPlaying && loopTrack == false) {
+			nextTrack = true;
+		} else if (loopTrack == true && triggerNextTrack == true) {
 			nextTrack = true;
 		}
-
+		
 		if (nextTrack == true) {
-			playTrack += 1;
+			if (restartTrack == true) {
+				restartTrack = false;
+			} else if (backTrack == true) {
+				playTrack -= 1;
+				backTrack = false;
+				if (playTrack < 0) {
+					playTrack = trackIndex;
+				}
+			} else {
+				playTrack += 1;
+			}
 			if (playTrack > trackIndex) {
 				playTrack = 0;
 			}
 			nextTrack = false;
+			triggerNextTrack = false;
 		}
+		
 		return playTrack;
 	}
-
+	
 	//Unity shortcuts for Music
 	void UMusicInput () {
-		 
+		
 		//Press M to Mute the current track
 		if (Input.GetKeyDown(KeyCode.M) && Mute == false) {
 			Mute = true;
@@ -72,35 +124,21 @@ public class jukeBox : MonoBehaviour {
 		if (Mute == false && thisJukeBox.mute == true) {
 			thisJukeBox.mute = false;
 		}
-
+		
 		//Press Right Arrow to skip to the next track.
 		if (Input.GetKeyDown (KeyCode.RightArrow) && thisJukeBox.isPlaying && skipTrack == false) {
 			skipTrack = true;
+			triggerNextTrack = true;
 		}
-	}
-
-
-
-	void Awake () {
-
-		//Get the Audio Source assigned to this game object
-		thisJukeBox = this.gameObject.GetComponent<AudioSource>();
-		Mute = false;
-		enableMusic = true; //This should be tied to a static global variable at some point.
-		tracks = Songs.Length;
-		Debug.Log("Number of Songs: " + tracks);
-		nextTrack = false;
-		skipTrack = false;
-	}
-	
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		playMusic();
-
+		
+		if (Input.GetKeyDown (KeyCode.DownArrow) && thisJukeBox.isPlaying && restartTrack == false) {
+			restartTrack = true;
+			thisJukeBox.Stop();
+		}
+		
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && thisJukeBox.isPlaying && backTrack == false) {
+			backTrack = true;
+			thisJukeBox.Stop ();
+		}
 	}
 }
